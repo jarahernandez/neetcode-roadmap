dd85ebac76ddb8c038244bed5c04ff194206505a
jarahernandez <ejarahernandez1@gmail.com>
2026-02-14 08:17:39 -0500

    work on two sum exercise

---
+++ arrays-and-hashing/two-sum.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const nums = [3, 2, 4];
+let target = 6;
+
+const twoSum = (nums, target) => {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i === j) {
+                continue;
+            } else {
+                if (nums[i] + nums[j] === target) {
+                    return [i, j];
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+};
+
+console.log(twoSum(nums, target));
